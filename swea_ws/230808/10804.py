42cab7107ce3f6bf7f79f29a01aec2a9e1f7222e
yuna jeong <jyn7077@naver.com>
2023-08-09 08:50:22 +0900

    ii

--- swea_ws/230808/10804.py
+++ swea_ws/230808/10804.py
@@ -1,10 +1,4 @@
-# def text_reverse(text, N):
-#     # reverse_list = []
-#     reverse_text = ''
-#     for r in range(N):
-#         reverse_text = text[r] + reverse_text
-#         # reverse_list.append(reverse_text)
-#     return list(reverse_text)
+# 문자열의 거울상
 
 
 TC = int(input())
@@ -15,21 +9,3 @@ for tc in range(1, TC+1):
     for i in range(len(txt)):
         new_text = txt_mirror[txt[i]] + new_text
     print(f'#{tc} {new_text}')
-# #
-# # txt3 = {'b':'d'}
-# # print(txt3['b'])
-
-# txt3 = {'b':'d', 'd':'b', 'q':'p', 'p':'q'}
-# print(str(txt3['b']))
-#
-#
-
-TC = int(input())
-for tc in range(1, TC+1):
-    txt = list(input())
-    # print(text_reverse(txt, len(txt)))
-    txt2 = text_reverse(txt, len(txt))
-    txt3 = {'b':'d', 'd':'b', 'q':'p', 'p':'q'}
-    for i in txt2:
-        txt2[i] = txt3[txt2[i]]+ new_text
-    print(f'#{tc}', ''.join(txt2))#
--- swea_ws/230808/4861.py
+++ swea_ws/230808/4861.py
@@ -1,90 +1,26 @@
 # 회문
 
-# def brute(text, N, M):
-#     for n in range(N):
-#         if text[n][:M//2] == text[n][:(M//2)-1:-1]:
-#             return text[n]
-#
-# TC = int(input())
-# for tc in range(1, TC+1):
-#     N, M = map(int, input().split())    # N =  전체 텍스트의  M = 찾을 패턴의 길이
-#     text =[(input()) for _ in range(N)]
-#     print(brute(text, N, M))
-#---------------------------------------------------------------------------------
-N = 20
-M = 13
-M_1 = round(M/2)
-text = [(input()) for _ in range(20)]
+def text_col(text, N):
+    col_list = []
+    for c in range(N):
+        col_text = ''
+        for r in range(N):
+            col_text = col_text + text[r][c]
+        col_list.append(col_text)
+    return col_list
 
-# for n in range(N):
-#     if text[n][:M//2] == text[n][:(M//2)-1:-1]:
-#         print(text[n])
+TC = int(input())
+for tc in range(1, TC+1):
+    N, M = map(int, input().split())    # N =  전체 텍스트의  M = 찾을 패턴의 길이
+    text = [(input()) for _ in range(N)]
+    text2 = text_col(text, N)
 
-reverse_list = []
-for c in range(N):
-    reverse_text = ''
-    for r in range(N):
-        reverse_text = reverse_text + text[r][c]
-    reverse_list.append(reverse_text)
-print(reverse_list)
-print(reverse_list[0][:M_1])
-print(reverse_list[0][M//2:M])
+    for i in range(N):
+        for j in range(N-M+1):
+            if text[i][j:j+M] == text[i][j:j+M][::-1]:
+                print(f'#{tc}', text[i][j:j+M])
 
- # for i in range(N):
- #    for j in range(N-M+1):
- #        if reverse_list[i][j:j+(M//2)] == reverse_list[i][j:j+(M//2-1):-1]:
- #            return reverse_list[i]
- #        if text[i][:M//2] == text[i][:(M//2)-1:-1]:
- #            return text[i]
-
-# == reverse_list[n][:(M//2)-1:-1]
-#-------------------------------------------------------------------------------------
-    # for n in range(N):
-    #     if reverse_text[n][:5]:
-    #         print(reverse_text[n])
-#-----------------------------------------------------------------------------------------
-# N = 10
-# M = 10
-# text = [(input()) for _ in range(10)]
-#
-#
-# for c in range(N):
-#     reverse_text = ''
-#     for r in range(N):
-#         reverse_text = reverse_text + text[r][c]
-#     print(reverse_text)
-
-# print(text[0][0:M//2])
-# print(text[0][:(M//2)-1:-1])
-# print(len(text))
-
-#------------------------------------------------------------------------------------
-# def brute(text, N, M):
-#
-#     reverse_list = []
-#     for c in range(N):
-#         reverse_text = ''
-#         for r in range(N):
-#             reverse_text = reverse_text + text[r][c]
-#         reverse_list.append(reverse_text)
-#
-#
-#     # for n in range(N):
-#     #     if reverse_list[n][:M//2] == reverse_list[n][:(M//2)-1:-1]:
-#     #         return reverse_list[n]
-#     #     if text[n][:M//2] == text[n][:(M//2)-1:-1]:
-#     #         return text[n]
-#
-#
-#     for i in range(N):
-#         for j in range(N-M+1):
-#             if reverse_list[i][j:j+(M//2)] == reverse_list[i][j:j+(M//2-1):-1]:
-#                 return reverse_list[i]
-#             if text[i][:M//2] == text[i][:(M//2)-1:-1]:
-#                 return text[i]
-#
-# TC = int(input())
-# for tc in range(1, TC+1):
-#     N, M = map(int, input().split())    # N =  전체 텍스트의  M = 찾을 패턴의 길이
-#     text = [(input()) for _ in range(N)]
-#     print(f'#{tc} {brute(text, N, M)}')
+    for i in range(N):
+        for j in range(N-M+1):
+            if text2[i][j:j + M] == text2[i][j:j + M][::-1]:
+                print(f'#{tc}', text2[i][j:j + M])
